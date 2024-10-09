48541c62c304e92d8f0a3ebac4a240e3c9b919bc
Bahare Riahi <bahare198924@gmail.com>
2024-10-09 17:56:02 +0000

    Updated Task1.sh

--- HW4/Task1.sh
+++ HW4/Task1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Find the process ID (PID) of infinite.sh and kill it
+
+# Get the PID of the running infinite.sh script
+pid=$(pgrep -f infinite.sh)
+
+# Check if the process is running
+if [ -z "$pid" ]; then
+    echo "infinite.sh is not running."
+else
+    # Kill the process
+    kill $pid
+    echo "Killed infinite.sh process with PID: $pid"
+fi
 
